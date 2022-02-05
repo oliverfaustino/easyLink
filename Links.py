0cf0cb32efdae0f4f074471d416595dadd24d0ea
Samuca0258 <samuelnascimento679@gmail.com>
2022-02-05 20:54:26 -0300

    Oia os links

--- Links.py
+++ Links.py
@@ -1,19 +1,28 @@
+# Programador: Samuca
+# Programa: Programa de link
+# Objetivo: armazenar links e depois enviar o link que foi solicitado 
+# Destino:  easyLink
+# Versão: alpha
+# Começo: 03/02/2022
+# Fim: 05/02/2022
 
-l_py = "https://olhardigital.com.br/2021/10/04/tira-duvidas/o-que-e-para-que-serve-o-python/"
-l_pr = "http://www.bosontreinamentos.com.br/programacao-em-python/guia-basico-da-funcao-print-em-python/#:~:text=A%20fun%C3%A7%C3%A3o%20print()%20em,dentro%20de%20arquivos%20de%20texto"  
-l_v = "https://blog.ffelix.eti.br/como-criar-variaveis-em-python/"
-l_in = "http://www.bosontreinamentos.com.br/programacao-em-python/entrada-de-dados-em-python-com-funcao-input/"
-l_str = "https://www.devmedia.com.br/tipos-de-dados-em-python-string/40669"
-l_fl = "https://docs.python.org/pt-br/3/tutorial/floatingpoint.html"
-l_int = "https://panda.ime.usp.br/pensepy/static/pensepy/02-Conceitos/conceitos.html"
-l_whi = "https://www.devmedia.com.br/python-estrutura-de-repeticao-while/38546"
-l_if = "https://pt.m.wikipedia.org/wiki/Estrutura_de_sele%C3%A7%C3%A3o"
-l_ope = "https://www.pythonprogressivo.net/2018/02/Operadores-Comparacao-Python.html?m=1"
+#Links armazenados sobre python:   
+vs_py = "https://olhardigital.com.br/2021/10/04/tira-duvidas/o-que-e-para-que-serve-o-python/"
+vs_pr = "http://www.bosontreinamentos.com.br/programacao-em-python/guia-basico-da-funcao-print-em-python/#:~:text=A%20fun%C3%A7%C3%A3o%20print()%20em,dentro%20de%20arquivos%20de%20texto"  
+vs_v = "https://blog.ffelix.eti.br/como-criar-variaveis-em-python/"
+vs_in = "http://www.bosontreinamentos.com.br/programacao-em-python/entrada-de-dados-em-python-com-funcao-input/"
+vs_str = "https://www.devmedia.com.br/tipos-de-dados-em-python-string/40669"
+vs_fl = "https://docs.python.org/pt-br/3/tutorial/floatingpoint.html"
+vs_int = "https://panda.ime.usp.br/pensepy/static/pensepy/02-Conceitos/conceitos.html"
+vs_whi = "https://www.devmedia.com.br/python-estrutura-de-repeticao-while/38546"
+vs_if = "https://pt.m.wikipedia.org/wiki/Estrutura_de_sele%C3%A7%C3%A3o"
+vs_ope = "https://www.pythonprogressivo.net/2018/02/Operadores-Comparacao-Python.html?m=1"
 
+#Função que printa os links:
 def teste(site):
  print(site)  
 
-teste(l_pr)
+teste(vs_if)
 
 
 
