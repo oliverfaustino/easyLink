f9156010312d25946ffd2285aadea7c4fefb1c1c
Samuca0258 <samuelnascimento679@gmail.com>
2022-02-06 16:20:42 -0300

    Arquivo de links

--- Links.py
+++ Links.py
@@ -6,22 +6,47 @@
 # Começo: 03/02/2022
 # Fim: 05/02/2022
 
-#Links armazenados sobre python:   
-vs_py = "https://olhardigital.com.br/2021/10/04/tira-duvidas/o-que-e-para-que-serve-o-python/"
-vs_pr = "http://www.bosontreinamentos.com.br/programacao-em-python/guia-basico-da-funcao-print-em-python/#:~:text=A%20fun%C3%A7%C3%A3o%20print()%20em,dentro%20de%20arquivos%20de%20texto"  
-vs_v = "https://blog.ffelix.eti.br/como-criar-variaveis-em-python/"
-vs_in = "http://www.bosontreinamentos.com.br/programacao-em-python/entrada-de-dados-em-python-com-funcao-input/"
-vs_str = "https://www.devmedia.com.br/tipos-de-dados-em-python-string/40669"
-vs_fl = "https://docs.python.org/pt-br/3/tutorial/floatingpoint.html"
-vs_int = "https://panda.ime.usp.br/pensepy/static/pensepy/02-Conceitos/conceitos.html"
-vs_whi = "https://www.devmedia.com.br/python-estrutura-de-repeticao-while/38546"
-vs_if = "https://pt.m.wikipedia.org/wiki/Estrutura_de_sele%C3%A7%C3%A3o"
-vs_ope = "https://www.pythonprogressivo.net/2018/02/Operadores-Comparacao-Python.html?m=1"
 
-#Função que printa os links:
+
+
+#Links armazenados sobre python:   
+vs_link_python = "https://olhardigital.com.br/2021/10/04/tira-duvidas/o-que-e-para-que-serve-o-python/"
+vs_link_print = "http://www.bosontreinamentos.com.br/programacao-em-python/guia-basico-da-funcao-print-em-python/#:~:text=A%20fun%C3%A7%C3%A3o%20print()%20em,dentro%20de%20arquivos%20de%20texto"  
+vs_link_sintaxe = "https://pt.m.wikipedia.org/wiki/Sintaxe_e_sem%C3%A2ntica_de_Python"
+vs_link_com_em_Python = "http://excript.com/python/comentarios-em-python.html"
+vs_link_tipo_de_dados = "http://excript.com/python/tipos-de-dados-python.html"
+vs_link_num = "https://algoritmosempython.com.br/cursos/programacao-python/tipos-basicos/"
+vs_link_conversao = "https://www.digitalocean.com/community/tutorials/how-to-convert-data-types-in-python-3-pt"
+vs_link_boleanos = "https://panda.ime.usp.br/pensepy/static/pensepy/06-Selecao/selecao.html"
+vs_link_lista = "https://algoritmosempython.com.br/cursos/programacao-python/listas/"
+vs_link_tuplas = "https://blog.betrybe.com/tecnologia/tuplas-em-python/"
+vs_link_conjuntos = "https://pythonhelp.wordpress.com/2013/06/18/conjuntos-em-python/"
+vs_link_dicionario = "https://kenzie.com.br/blog/dicionario-python/"
+vs_link_loop_em_python = "https://blog.grancursosonline.com.br/python-loop/"
+vs_link_Funções_do_Python = "https://panda.ime.usp.br/pensepy/static/pensepy/05-Funcoes/funcoes.html#:~:text=Em%20Python%2C%20uma%20fun%C3%A7%C3%A3o%20%C3%A9,imaginamos%20uma%20solu%C3%A7%C3%A3o%20do%20problema"
+vs_link_variavel = "https://blog.ffelix.eti.br/como-criar-variaveis-em-python/"
+vs_link_input = "http://www.bosontreinamentos.com.br/programacao-em-python/entrada-de-dados-em-python-com-funcao-input/"
+vs_link_string = "https://www.devmedia.com.br/tipos-de-dados-em-python-string/40669"
+vs_link_float = "https://docs.python.org/pt-br/3/tutorial/floatingpoint.html"
+vs_link_integer = "https://panda.ime.usp.br/pensepy/static/pensepy/02-Conceitos/conceitos.html"
+vs_link_while = "https://www.devmedia.com.br/python-estrutura-de-repeticao-while/38546"
+vs_link_if_else = "https://pt.m.wikipedia.org/wiki/Estrutura_de_sele%C3%A7%C3%A3o"
+vs_link_oper_de_comp = "https://www.pythonprogressivo.net/2018/02/Operadores-Comparacao-Python.html?m=1"
+#v = v = abreviação para "variavel
+#s = abreviacao para string
+#vs = abreviacao para variavel do tipo string
+#link = link  
+#def = função 
+#com = comentarios
+#oper = operadores 
+#comp = comparação
+
+
+#Função que ira printa os links:
 def teste(site):
  print(site)  
 
+#Comando que executara a funcao (escolha o link  e digite entre os parenteses)
 teste(vs_if)
 
 
