34ff499e13aeee0c37cbc615da0798064ab12c72
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-02-07 21:28:20 -0300

    Alterações: adicionado o timer, conserto e organização

--- modulos/splash_screen.py
+++ modulos/splash_screen.py
@@ -1,47 +1,58 @@
 # Programador: Wender Brito
 # Programa: Splash Screen
-# Destino: easyLink
 # Versão: Alpha
 # Data/criação: 03/02/2022
-# Data/término: ...
-
 
 from tkinter import *
-import os 
+import os
+import time
 
-def f_splash_screen():
-    #Localizar o programa e consequentemente a imagem
-    v_pasta_tela=os.path.dirname(__file__)
+''' 
+Função: f_splash_screen
 
-    # dimensões da janela(vi)
-    vi_largura = 700
-    vi_altura = 350 
+Parâmetros:
+    1: largura
+    2: altura
+
+Exemplo de uso:
+f_splash_screen(300, 500)
+''' 
+v_tela= Tk()
+
+def f_fechar_janela():
+    v_tela.destroy()
+    return
+
+def f_splash_screen(p_largura= 700, p_altura= 350, p_v_imagem= "\\img_splash_screen.png", p_time= 6000):
+    #  CONSTANT
+    CAMINHO_ARQUIVO=os.path.dirname(__file__)
+
+    # Propriedades do Splash Screen
+    v_largura = p_largura
+    v_altura = p_altura
+    v_imagem=CAMINHO_ARQUIVO+p_v_imagem
 
     #Criar a tela
-    v_tela= Tk()
+    v_tela.after(p_time, f_fechar_janela)
     v_tela.title("Splash Screen")
-    # vs_tela.geometry("577x561")  
     v_tela.wm_resizable(width=False, height=False) 
 
     # resolução do nosso sistema (do meu pc )
-    vi_largura_screen = v_tela.winfo_screenwidth()
-    vi_altura_screen = v_tela.winfo_screenheight() 
+    v_largura_screen = v_tela.winfo_screenwidth()
+    v_altura_screen = v_tela.winfo_screenheight() 
 
     # posição da jav_janela (calculo para centralizar a jav_janela )
-    v_posx = vi_largura_screen/2 - vi_largura/2 
-    v_posy = vi_altura_screen/2 - vi_altura/2
+    v_posx = v_largura_screen/2 - v_largura/2 
+    v_posy = v_altura_screen/2 - v_altura/2
 
     # definir geometry (utilizando o calculo para centralizar a jav_janela )
-    v_tela.geometry("%dx%d+%d+%d"%(vi_largura,vi_altura,v_posx,v_posy))
+    v_tela.geometry("%dx%d+%d+%d"%(v_largura,v_altura,v_posx,v_posy))
 
-    #Importa imagem
-    vs_img_splash=PhotoImage(file=v_pasta_tela+"\\img_splash_screen.png")
+    #Importa v_imagem
+    v_img_splash=PhotoImage(file= v_imagem)
 
     #Label
-    vs_label_img=Label(v_tela,image=vs_img_splash)
+    vs_label_img=Label(v_tela,image=v_img_splash)
     vs_label_img.place(x=0,y=0)
     mainloop()
     return 
-
-f_splash_screen()
-
